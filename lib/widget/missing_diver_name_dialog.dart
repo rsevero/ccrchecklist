1c4d05b8fa34cbcddbf0b73a0a4ce709df4af978
Rodrigo Severo <rsev@pm.me>
2024-02-04 12:43:13 -0300

    diver name dialog's TextField getting focus and enter key submiting

--- lib/widget/missing_diver_name_dialog.dart
+++ lib/widget/missing_diver_name_dialog.dart
@@ -11,6 +11,28 @@ class MissingDiverNameDialog extends StatefulWidget {
 
 class _MissingDiverNameDialogState extends State<MissingDiverNameDialog> {
   final TextEditingController _controller = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    // Automatically focus the TextField when the dialog is displayed
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _focusNode.requestFocus();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _handleSubmit() {
+    widget.onDiverNameSubmitted(_controller.text);
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +40,14 @@ class _MissingDiverNameDialogState extends State<MissingDiverNameDialog> {
       title: const Text('Missing Information'),
       content: TextField(
         controller: _controller,
+        focusNode: _focusNode,
         decoration: const InputDecoration(hintText: 'Enter Diver Name'),
+        onSubmitted: (_) => _handleSubmit(),
       ),
       actions: <Widget>[
         TextButton(
+          onPressed: _handleSubmit,
           child: const Text('Submit'),
-          onPressed: () {
-            widget.onDiverNameSubmitted(_controller.text);
-            Navigator.of(context).pop();
-          },
         ),
       ],
     );
