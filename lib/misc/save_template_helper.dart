f96ca0ec94b0881e38a84901dd06832cfcb01968
Rodrigo Severo <rsev@pm.me>
2024-10-15 16:06:30 -0300

    SaveTempalteHelper: removing deprecated code

--- lib/misc/save_template_helper.dart
+++ lib/misc/save_template_helper.dart
@@ -4,41 +4,9 @@ import 'package:ccr_checklist/misc/constants.dart';
 import 'package:ccr_checklist/misc/ccr_directory_helper.dart';
 import 'package:ccr_checklist/store/template_editor_store.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:slugify/slugify.dart';
 import 'package:uuid/uuid.dart';
-
-Future<CCRFileExistsAction?> ccrShowFileExistsDialog(
-    BuildContext context, String filepath) async {
-  return await showDialog<CCRFileExistsAction>(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('File "$filepath" already exists'),
-        content: const Text(
-            'Do you want to replace it, choose another name, or cancel?'),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('Replace'),
-            onPressed: () =>
-                Navigator.of(context).pop(CCRFileExistsAction.replace),
-          ),
-          TextButton(
-            child: const Text('Choose another'),
-            onPressed: () =>
-                Navigator.of(context).pop(CCRFileExistsAction.chooseAnother),
-          ),
-          TextButton(
-            child: const Text('Cancel'),
-            onPressed: () =>
-                Navigator.of(context).pop(CCRFileExistsAction.cancel),
-          ),
-        ],
-      );
-    },
-  );
-}
 
 Future<bool> fileExists(String filepath) async {
   final file = File(filepath);
@@ -59,50 +27,27 @@ Future<String> ccrFileNameFromTemplate(Template template) async {
   return filename;
 }
 
-Future<String> ccrFilePathFromFileName(String fileName) async {
-  if (fileName.endsWith('.$ccrTemplateExtension')) {
-    fileName = fileName.substring(
-        0, fileName.length - (ccrTemplateExtension.length + 1));
+Future<String> ccrFilePathFromFileName(String filename) async {
+  if (filename.endsWith('.$ccrTemplateExtension')) {
+    filename = filename.substring(
+        0, filename.length - (ccrTemplateExtension.length + 1));
   }
   final directory = await CCRDirectory.templates();
-  final filePath = '${directory.path}/$fileName.$ccrTemplateExtension';
+  final filepath = '${directory.path}/$filename.$ccrTemplateExtension';
 
-  return filePath;
+  return filepath;
 }
 
 Future<void> ccrSaveAsTemplate(BuildContext context, Template template,
-    [String? fileName = '']) async {
-  if ((fileName == null) || (fileName == '')) {
-    fileName = await ccrFileNameFromTemplate(template);
+    [String? filename = '']) async {
+  if ((filename == null) || (filename == '')) {
+    filename = await ccrFileNameFromTemplate(template);
   }
 
-  String filePath = await ccrFilePathFromFileName(fileName);
-  File file = File(filePath);
-
-  fileExistsLoop:
-  while (await file.exists()) {
-    if (!context.mounted) return;
-    final action = await ccrShowFileExistsDialog(context, fileName!);
-    switch (action) {
-      case CCRFileExistsAction.replace:
-        break fileExistsLoop;
-      case CCRFileExistsAction.chooseAnother:
-        if (!context.mounted) return;
-        fileName = await ccrFileNameFromTemplate(template);
-        if (fileName == '') {
-          return;
-        }
-        filePath = await ccrFilePathFromFileName(fileName);
-        file = File(filePath);
-        break;
-      case CCRFileExistsAction.cancel:
-      default:
-        return;
-    }
-  }
+  final String filepath = await ccrFilePathFromFileName(filename);
 
   if (!context.mounted) return;
   final templateEditorStore =
       Provider.of<TemplateEditorStore>(context, listen: false);
-  templateEditorStore.saveTemplate(context, template, filePath);
+  templateEditorStore.saveTemplate(context, template, filepath);
 }
