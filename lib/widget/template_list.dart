bc2497cbef803dfca225cd9a0a3a48aded6dd3a6
Rodrigo Severo <rsev@pm.me>
2024-10-15 15:03:18 -0300

    TemplateList: formating and highlighting selected options

--- lib/widget/template_list.dart
+++ lib/widget/template_list.dart
@@ -1,12 +1,11 @@
 import 'package:ccr_checklist/data/template_file.dart';
 import 'package:ccr_checklist/store/template_list_store.dart';
 import 'package:ccr_checklist/widget/template_list_tile_widget.dart';
-import 'package:expansion_tile_list/expansion_tile_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
-class TemplateList extends StatelessWidget {
+class TemplateList extends StatefulWidget {
   final Function(BuildContext, int) onTapTemplateFile;
   final Function(BuildContext, int)? onRemoveTemplateFile;
   final bool isEditor;
@@ -19,42 +18,76 @@ class TemplateList extends StatelessWidget {
   });
 
   @override
+  State<TemplateList> createState() => _TemplateListState();
+}
+
+class _TemplateListState extends State<TemplateList> {
+  late TextStyle manufacturerTextTheme;
+  late TextStyle modelTextTheme;
+  late Color expandedManufacturerColor;
+  late Color expandedModelColor;
+  late Color expandedManufacturerTextColor;
+  late Color expandedModelTextColor;
+  late Color collapsedManufacturerTextColor;
+  late Color collapsedModelTextColor;
+  late TextStyle templateTitleTextTheme;
+  late TextStyle templateDescriptionTextTheme;
+  late TemplateListStore templateListStore;
+  late List<TemplateFile> defaultTemplates;
+  late String currentManufacturer;
+  late String currentModel;
+  String expandedManufacturer = '';
+  String expandedModel = '';
+
+  @override
   Widget build(BuildContext context) {
-    final templateListStore = Provider.of<TemplateListStore>(context);
+    templateListStore = Provider.of<TemplateListStore>(context);
     templateListStore.update();
+    currentManufacturer = '';
+    currentModel = '';
 
     return Observer(
       builder: (_) {
-        if (templateListStore.state != TemplateListStoreState.uptodate) {
-          return const Center(child: CircularProgressIndicator());
-        }
+        final observableDefaultTemplates = templateListStore.defaultTemplates;
 
-        final defaultTemplates = templateListStore.defaultTemplates.toList();
-        return ExpansionTileList.radio(
-          children: _buildDefaultTile(context, defaultTemplates),
+        defaultTemplates = observableDefaultTemplates.toList();
+
+        return ListView(
+          children: _buildDefaultTile(context),
         );
       },
     );
   }
 
-  List<ExpansionTile> _buildDefaultTile(
-      BuildContext context, List<TemplateFile> defaultTemplates) {
+  List<ExpansionTile> _buildDefaultTile(BuildContext context) {
     List<ExpansionTile> manufacturers = [];
-    List<Widget> models = [];
+    List<ExpansionTile> models = [];
     List<Widget> templates = [];
 
-    String currentManufacturer = '';
-    String currentModel = '';
+    final ThemeData currentTheme = Theme.of(context);
 
-    final TextTheme currentTextTheme = Theme.of(context).textTheme;
-    final TextStyle manufacturerTextTheme =
+    final ColorScheme currentColorScheme = currentTheme.colorScheme;
+    expandedModelColor = currentColorScheme.secondaryContainer;
+    expandedModelTextColor = currentColorScheme.onSecondaryContainer;
+    expandedManufacturerColor = currentColorScheme.secondary;
+    expandedManufacturerTextColor = currentColorScheme.onSecondary;
+    collapsedManufacturerTextColor = currentColorScheme.onSurface;
+    collapsedModelTextColor = currentColorScheme.onSurface;
+
+    final TextTheme currentTextTheme = currentTheme.textTheme;
+    manufacturerTextTheme =
         currentTextTheme.headlineSmall!.copyWith(fontWeight: FontWeight.w200);
-    final TextStyle modelTextTheme =
+    manufacturerTextTheme = _copyUnsettingColor(manufacturerTextTheme);
+    modelTextTheme =
         currentTextTheme.titleLarge!.copyWith(fontWeight: FontWeight.w500);
-    final TextStyle templateTitleTextTheme =
+    modelTextTheme = _copyUnsettingColor(modelTextTheme);
+    templateTitleTextTheme =
         currentTextTheme.titleMedium!.copyWith(fontWeight: FontWeight.w700);
-    final TextStyle templateDescriptionTextTheme =
+    templateTitleTextTheme = _copyUnsettingColor(templateTitleTextTheme);
+    templateDescriptionTextTheme =
         currentTextTheme.bodyLarge!.copyWith(fontWeight: FontWeight.w300);
+    templateDescriptionTextTheme =
+        _copyUnsettingColor(templateDescriptionTextTheme);
 
     for (var templateIndex = 0;
         templateIndex < defaultTemplates.length;
@@ -62,33 +95,11 @@ class TemplateList extends StatelessWidget {
       final template = defaultTemplates[templateIndex];
       if (template.rebreatherManufacturer != currentManufacturer) {
         if (currentManufacturer != '') {
-          models.add(
-            ExpansionTile(
-              title: Text(
-                currentModel,
-                style: modelTextTheme,
-              ),
-              children: templates,
-            ),
-          );
+          models.add(_modelTile(templates));
           if (models.length == 1) {
-            models[0] = _expandTile(models[0] as ExpansionTile);
-          } else {
-            models = [
-              ExpansionTileList.radio(
-                children: models.map((e) => e as ExpansionTile).toList(),
-              ),
-            ];
+            models[0] = _expandTile(models[0]);
           }
-          manufacturers.add(
-            ExpansionTile(
-              title: Text(
-                currentManufacturer,
-                style: manufacturerTextTheme,
-              ),
-              children: models,
-            ),
-          );
+          manufacturers.add(_manufacturerTile(models));
         }
         currentManufacturer = template.rebreatherManufacturer;
         models = [];
@@ -96,15 +107,7 @@ class TemplateList extends StatelessWidget {
         templates = [];
       } else if (template.rebreatherModel != currentModel) {
         if (currentModel != '') {
-          models.add(
-            ExpansionTile(
-              title: Text(
-                currentModel,
-                style: modelTextTheme,
-              ),
-              children: templates,
-            ),
-          );
+          models.add(_modelTile(templates));
         }
         currentModel = template.rebreatherModel;
         templates = [];
@@ -115,47 +118,25 @@ class TemplateList extends StatelessWidget {
           description:
               Text(template.description, style: templateDescriptionTextTheme),
           isAsset: template.isAsset,
-          isEditor: isEditor,
+          isEditor: widget.isEditor,
           templateIndex: templateIndex,
           onTap: () {
-            onTapTemplateFile(context, templateIndex);
+            widget.onTapTemplateFile(context, templateIndex);
           },
-          onRemove: onRemoveTemplateFile,
+          onRemove: widget.onRemoveTemplateFile,
         ),
       );
     }
 
     if (templates.isNotEmpty) {
-      models.add(
-        ExpansionTile(
-          title: Text(
-            currentModel,
-            style: modelTextTheme,
-          ),
-          children: templates,
-        ),
-      );
+      models.add(_modelTile(templates));
     }
 
     if (models.isNotEmpty) {
       if (models.length == 1) {
-        models[0] = _expandTile(models[0] as ExpansionTile);
-      } else {
-        models = [
-          ExpansionTileList.radio(
-            children: models.map((e) => e as ExpansionTile).toList(),
-          ),
-        ];
+        models[0] = _expandTile(models[0]);
       }
-      manufacturers.add(
-        ExpansionTile(
-          title: Text(
-            currentManufacturer,
-            style: manufacturerTextTheme,
-          ),
-          children: models,
-        ),
-      );
+      manufacturers.add(_manufacturerTile(models));
     }
 
     if (manufacturers.length == 1) {
@@ -165,11 +146,81 @@ class TemplateList extends StatelessWidget {
     return manufacturers;
   }
 
+  TextStyle _copyUnsettingColor(TextStyle style) {
+    final TextStyle uncoloredStyle = TextStyle(
+      background: style.background,
+      // color: style.color,
+      debugLabel: style.debugLabel,
+      decoration: style.decoration,
+      decorationColor: style.decorationColor,
+      decorationStyle: style.decorationStyle,
+      decorationThickness: style.decorationThickness,
+      fontFamily: style.fontFamily,
+      fontFamilyFallback: style.fontFamilyFallback,
+      fontFeatures: style.fontFeatures,
+      fontSize: style.fontSize,
+      fontStyle: style.fontStyle,
+      fontVariations: style.fontVariations,
+      fontWeight: style.fontWeight,
+      foreground: style.foreground,
+      height: style.height,
+      // inherit: style.inherit,
+      leadingDistribution: style.leadingDistribution,
+      letterSpacing: style.letterSpacing,
+      locale: style.locale,
+      overflow: style.overflow,
+      shadows: style.shadows,
+      textBaseline: style.textBaseline,
+      wordSpacing: style.wordSpacing,
+    );
+    return uncoloredStyle;
+  }
+
   ExpansionTile _expandTile(ExpansionTile tile) {
-    return ExpansionTile(
+    final expandedTile = ExpansionTile(
       title: tile.title,
+      backgroundColor: tile.backgroundColor,
+      textColor: tile.textColor,
+      collapsedTextColor: tile.collapsedTextColor,
+      iconColor: tile.iconColor,
       initiallyExpanded: true,
       children: tile.children,
     );
+
+    return expandedTile;
+  }
+
+  ExpansionTile _modelTile(List<Widget> templates) {
+    final ExpansionTile newModel = ExpansionTile(
+      title: Text(
+        currentModel,
+        style: modelTextTheme,
+      ),
+      backgroundColor: expandedModelColor,
+      textColor: expandedModelTextColor,
+      collapsedTextColor: collapsedModelTextColor,
+      iconColor: expandedModelTextColor,
+      collapsedIconColor: collapsedModelTextColor,
+      children: templates,
+    );
+
+    return newModel;
+  }
+
+  ExpansionTile _manufacturerTile(List<Widget> models) {
+    final ExpansionTile newManufacturer = ExpansionTile(
+      title: Text(
+        currentManufacturer,
+        style: manufacturerTextTheme,
+      ),
+      backgroundColor: expandedManufacturerColor,
+      textColor: expandedManufacturerTextColor,
+      collapsedTextColor: collapsedManufacturerTextColor,
+      iconColor: expandedManufacturerTextColor,
+      collapsedIconColor: collapsedManufacturerTextColor,
+      children: models,
+    );
+
+    return newManufacturer;
   }
 }
