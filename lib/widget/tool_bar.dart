d5bb881da1b7cfdd06b783069acda2bc2b0e22a2
Rodrigo Severo <rsev@pm.me>
2024-11-19 06:38:20 -0300

    Make actions and contents optional in ToolBar

--- lib/widget/tool_bar.dart
+++ lib/widget/tool_bar.dart
@@ -3,30 +3,31 @@ import 'package:ccrchecklist/misc/flutter_extension_methods.dart';
 import 'package:flutter/material.dart';
 
 class ToolBar extends StatelessWidget {
-  final List<Widget> actions;
+  final List<Widget>? actions;
   final List<Widget> contents;
 
   const ToolBar({
     super.key,
-    required this.actions,
-    required this.contents,
+    this.actions,
+    this.contents = const [],
   });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8.0),
-              decoration: BoxDecoration(
-                color: context.ccrThemeExtension.surfaceDim,
-                borderRadius: ccrTemplateListTileBorderRadius,
+            if (actions != null)
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: context.ccrThemeExtension.surfaceDim,
+                  borderRadius: ccrTemplateListTileBorderRadius,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: actions!,
+                ),
               ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: actions,
-              ),
-            ),
           ] +
           contents,
     );
