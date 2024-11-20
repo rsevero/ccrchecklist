68f6253f81ea414248181ba6c56aa9bbab3466c3
Rodrigo Severo <rsev@pm.me>
2024-11-20 17:02:48 -0300

    Migrating ChecklistPage AppBar actions to ToolBar

--- lib/page/checklist_page.dart
+++ lib/page/checklist_page.dart
@@ -14,15 +14,19 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import 'package:ccrchecklist/misc/about_dialog_helper.dart';
 import 'package:ccrchecklist/misc/flutter_extension_methods.dart';
+import 'package:ccrchecklist/misc/help_dialog_helper.dart';
 import 'package:ccrchecklist/misc/helper_functions.dart';
 import 'package:ccrchecklist/page/checklist_complete_page.dart';
 import 'package:ccrchecklist/store/checklist_editor_store.dart';
-import 'package:ccrchecklist/widget/checklist_appbar.dart';
+import 'package:ccrchecklist/store/config_store.dart';
 import 'package:ccrchecklist/widget/checklist_body.dart';
 import 'package:ccrchecklist/widget/tool_bar.dart';
 import 'package:flutter/material.dart'; // For date formatting
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class ChecklistPage extends StatelessWidget {
@@ -33,6 +37,7 @@ class ChecklistPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final checklistEditorStore = Provider.of<ChecklistEditorStore>(context);
+    final ConfigStore configStore = Provider.of<ConfigStore>(context);
     final totalSections = checklistEditorStore.sections.length;
     final theme = context.ccrThemeExtension;
 
@@ -50,13 +55,56 @@ class ChecklistPage extends StatelessWidget {
       },
       child: SafeArea(
         child: Scaffold(
-          appBar: CheckListAppBar(
-            title: checklistEditorStore.title,
-            description: checklistEditorStore.description,
-            rebreatherManufacturer: checklistEditorStore.rebreatherManufacturer,
-            rebreatherModel: checklistEditorStore.rebreatherModel,
+          appBar: AppBar(
+            title: const Text('Checklist'),
+            // title: checklistEditorStore.title,
+            // description: checklistEditorStore.description,
+            // rebreatherManufacturer: checklistEditorStore.rebreatherManufacturer,
+            // rebreatherModel: checklistEditorStore.rebreatherModel,
           ),
           body: ToolBar(
+            actions: [
+              Observer(
+                builder: (_) => IconButton(
+                  icon: const Icon(Icons.refresh),
+                  tooltip: 'Reset Checklist',
+                  onPressed: checklistEditorStore.checklistChanged
+                      ? () => onPressedResetChecklist(context)
+                      : null,
+                ),
+              ),
+              IconButton(
+                icon: HugeIcon(
+                  icon: HugeIcons.strokeRoundedZoomInArea,
+                  color: Colors.black,
+                  size: 24.0,
+                ),
+                tooltip: 'Info',
+                onPressed: () {
+                  _showInfoDialog(
+                    context,
+                    checklistEditorStore.title,
+                    checklistEditorStore.description,
+                    checklistEditorStore.rebreatherManufacturer,
+                    checklistEditorStore.rebreatherModel,
+                    configStore.diverName,
+                    checklistEditorStore.date,
+                  );
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.help_outline),
+                onPressed: () => ccrOpenHelpDialog(context, 'ChecklistPage'),
+                tooltip: 'Help',
+              ),
+              IconButton(
+                icon: const Icon(Icons.info_outline),
+                onPressed: () {
+                  showCCRAboutDialog(context);
+                },
+                tooltip: 'About',
+              ),
+            ],
             content: ChecklistBody(sectionIndex: sectionIndex),
           ),
           bottomNavigationBar: ClipRRect(
@@ -170,6 +218,115 @@ class ChecklistPage extends StatelessWidget {
           builder: (context) => const ChecklistCompletePage(),
         ),
       );
+    }
+  }
+
+  void _showInfoDialog(
+    BuildContext context,
+    String title,
+    String description,
+    String rebreatherManufacturer,
+    String rebreatherModel,
+    String diverName,
+    DateTime date,
+  ) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Checklist Info',
+            style: context.textTheme.headlineMedium,
+          ),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Title',
+                style: context.textTheme.bodyMedium,
+              ),
+              Text(
+                title,
+                style: context.textTheme.titleLarge
+                    ?.copyWith(fontWeight: FontWeight.w700),
+              ),
+              SizedBox(height: context.textTheme.bodySmall?.fontSize ?? 8),
+              Text(
+                'Description',
+                style: context.textTheme.bodyMedium,
+              ),
+              Text(
+                description,
+                style: context.textTheme.titleMedium,
+              ),
+              SizedBox(height: context.textTheme.bodySmall?.fontSize ?? 8),
+              Text(
+                'Manufacturer',
+                style: context.textTheme.bodyMedium,
+              ),
+              Text(
+                rebreatherManufacturer,
+                style: context.textTheme.titleMedium,
+              ),
+              SizedBox(height: context.textTheme.bodySmall?.fontSize ?? 8),
+              Text(
+                'Model',
+                style: context.textTheme.bodyMedium,
+              ),
+              Text(
+                rebreatherModel,
+                style: context.textTheme.titleMedium,
+              ),
+              SizedBox(height: context.textTheme.bodySmall?.fontSize ?? 8),
+              Text(
+                'Diver',
+                style: context.textTheme.bodyMedium,
+              ),
+              Text(
+                diverName,
+                style: context.textTheme.titleSmall,
+              ),
+              SizedBox(height: context.textTheme.bodySmall?.fontSize ?? 8),
+              Text(
+                'Creation date',
+                style: context.textTheme.bodyMedium,
+              ),
+              Text(
+                DateFormat('yyyy-MM-dd – kk:mm').format(date),
+                style: context.textTheme.titleSmall,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void onPressedResetChecklist(BuildContext context) async {
+    final checklistEditorStore =
+        Provider.of<ChecklistEditorStore>(context, listen: false);
+
+    bool confirm = await ccrConfirmActionDialog(context, "Reset Checklist",
+        "Are you sure you want to reset the checklist and lose all changes?");
+
+    if (confirm) {
+      if (!context.mounted) return;
+      final result = await checklistEditorStore.resetChecklist(context);
+
+      if (!result) {
+        return;
+      }
+      if (!context.mounted) return;
+      navigateToSection(context, 0);
     }
   }
 }
